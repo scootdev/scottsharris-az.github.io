70a6cfdb3c17d056af8a54a84d8661dae8078b82
Scott Harris <scottsharris@yahoo.com>
2020-08-28 18:54:51 -0700

    Updates

---
+++ precache-manifest.97c8dedb072d186414bc8eb13176fc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d19d40b6f17301e2b87c8cecb63104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5a1e1d42a51959100fa",
+    "url": "/static/css/main.08a64a9b.chunk.css"
+  },
+  {
+    "revision": "aa4d9f59bef20d32e77e",
+    "url": "/static/js/2.6e34b444.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.6e34b444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a1e1d42a51959100fa",
+    "url": "/static/js/main.a7915759.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
